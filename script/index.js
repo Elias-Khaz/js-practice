f04d3d8cc38bb3e0e8c1105f84701390a930751e
Elias-Khaz <elias.khazroun5@gmail.com>
2025-10-15 10:42:56 +0200

    finished all exercices

---
+++ script/index.js
@@ -0,0 +1,21 @@
+// Exercise 1
+let hw = 'Hello World!';
+
+console.log(hw);
+
+// Exercise 2
+let name = 'Elias';
+let age = '20yrs';
+let favColor = 'Green'
+
+console.log(`Hello, my name is ${name} I am ${age} and I like the color ${favColor}`);
+
+// Exercise 3
+let num1 = 20;
+let num2 = 5;
+
+console.log(`Addition: ${num1} + ${num2} = ${num1} + ${num2}`)
+console.log(`Subtraction: ${num1} - ${num2} = ${num1} - ${num2}`)
+console.log(`Multiplication: ${num1} * ${num2} = ${num1} * ${num2}`)
+console.log(`Division: ${num1} / ${num2} = ${num1} / ${num2}`)
+
